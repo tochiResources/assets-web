7bed74dff9959353e0657fa6a7f87d3d00651217
Tochi <tochi_osu@hotmail.com>
2024-01-17 12:10:44 +0100

    update `index.js`

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const Logo = require('./src/Classes/Logo');
 const SFX = require('./src/Classes/SFX');
 const Cursors = require('@tochiresources/cursors');
 
-try { require('./src/compile')(); } catch (e) {}
+// try { require('./src/compile')(); } catch (e) {}
 
 module.exports = {
     CSS: CSS,
